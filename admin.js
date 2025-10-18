1d26a1659850d67291bc0250a51265f2a444448b
MauriWelter <mauriwelter.mjw@gmail.com>
2025-10-18 00:39:16 -0300

    atualizacao

--- admin.js
+++
@@ -1,6 +0,0 @@
-const btnServicos = document.getElementById('btnServicos');
-const submenu = document.getElementById('submenu');
-
-btnServicos.addEventListener('click', () => {
-    submenu.classList.toggle('hidden');
-});
